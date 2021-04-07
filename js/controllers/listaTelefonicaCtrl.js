230a4d39452c1993956ea32a601b21c25bf0013a
Katia Marina Silva <ktiax13@gmail.com>
2021-04-07 17:24:39 -0300

    nameFilter - v1.5.3

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,5 +1,5 @@
 angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, contatosAPI, operadorasAPI, serialGenerator, uppercaseFilter) {
-	console.log(serialGenerator.generate());
+	// console.log(serialGenerator.generate());
 	$scope.app = "Lista Telefonica";
 	$scope.contatos = [];
 	$scope.operadoras = [];
