e5748ca04319ada5399dbb2685f8096f15c79ac2
Katia Marina Silva <ktiax13@gmail.com>
2021-04-08 09:20:59 -0300

    replace, restrict, scope e vinculação - v1.5.6

--- js/directives/uiAlertDirective.js
+++ js/directives/uiAlertDirective.js
@@ -1,6 +1,20 @@
 angular.module("listaTelefonica").directive("uiAlert", function (){
     return {        
         // template: "<div>uiAlert</div>"
-        templateUrl: "view/alert.html"
+        templateUrl: "view/alert.html", 
+        replace: true,
+     //   restrict: "A", //funciona pra atributo, <div ui-alert></div>, não <ui-alert></ui-alert>
+       // restrict: "E", //funciona pra elemento, <ui-alert></ui-alert>, não <div ui-alert></div>
+       restrict: "AE", //combinaço para que ambos funcionem
+       //C para restriçaõ de class
+       //M para restriço de comentário
+       scope: {
+           //se o nome da propriedade da diretiva e do atributo são iguais, basta pôr o @ ou =
+        //    topic: "@title"
+        title: "@",
+        //= two way data binding
+        message: "="
+
+       }
     }
 });
